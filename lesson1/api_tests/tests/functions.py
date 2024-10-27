0fa4ad251f2834df369db9c2c7f31005f24d3976
DariaMartinovskaya <daria.martinovskaya@gmail.com>
2024-10-27 14:09:13 +0000

    Add/Update functions.py

---
+++ lesson1/api_tests/tests/functions.py
@@ -0,0 +1,26 @@
+import requests
+
+from pprint import pprint
+
+URL_CREATE = "http://127.0.0.1:8000/testcases/"
+
+def create_new_test_case():
+    info = {
+    "id": 0,
+    "name": "TestCasePOST",
+    "description": "This test checks if the test case is created",
+    "steps": [
+      "Step 1: Fill in all fields with valid data. Step 2: Click the Add Test Case button."
+    ],
+    "expected_result": "Test Case is created and listed",
+    "priority": "высокий"
+    }
+
+    response = requests.post(URL_CREATE, json=info)
+
+    return response
+
+response = create_new_test_case()
+print(response.status_code)
+print()
+pprint(response.json())
