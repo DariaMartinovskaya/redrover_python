103c01895e2cb96703299647863e877949445206
DariaMartinovskaya <daria.martinovskaya@gmail.com>
2024-11-03 17:06:47 +0000

    lesson 2 my update

--- lesson2/simple_example.py
+++ lesson2/simple_example.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 browser = webdriver.Chrome()
 browser.get("http://195.133.27.184/")
-element = browser.find_element(By.XPATH, "//a[@href='/2/']")
+element = browser.find_element(By.XPATH, "/html/body/main/div[1]/div/section[1]/div/a[1]")
 element.click()
 
-assert browser.current_url == "http://195.133.27.184/3/", "Wrong URL"
+time.sleep(4)
+
+assert browser.current_url == "http://195.133.27.184/list/", "Wrong URL"
+
+#assert element.is_displayed(), "Element is not visible"
